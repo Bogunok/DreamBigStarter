f0186c4651c508fab774f1b949ba210243fd2718
Bogunok <elizaveta.bogun04@gmail.com>
2024-10-12 22:13:31 +0300

    created starter project

---
+++ src/main/java/com/example/dreambigstarter/DreamBigStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.dreambigstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamBigStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamBigStarterApplication.class, args);
+    }
+
+}
